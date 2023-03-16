1130f484635d69f9d55f81379df855441357ebc1
Arnaud <arnaud.mengelle@orange.fr>
2023-03-16 15:20:57 +0100

    Marche vraiment

--- interface.py
+++ interface.py
@@ -5,7 +5,7 @@ from tkinter import messagebox
 #edf4ed
 #a8bba0
 #d0d9c8
-def getAll(listComboSexe, listComboCoulCheveux, listComboBarbe, listComboCheveux, listComboNez, listComboSourcils, listComboLunettes):
+def getAll():
     a=listComboSexe.get()
     b=listComboCoulCheveux.get()
     c=listComboBarbe.get()
@@ -19,72 +19,70 @@ def getAll(listComboSexe, listComboCoulCheveux, listComboBarbe, listComboCheveux
         messagebox.showerror(title="Erreur de selection", message="Un individu chauve n'a pas de couleur de cheveux veuillez selectionner Autre en couleur de cheveux ")
     selectCaract.destroy()
 
-def SelectCaractere():
-    selectCaract = Tk()
-    selectCaract.title('Accueil')
-    selectCaract.geometry('1300x800')
 
-    #grid
-    selectCaract.grid_rowconfigure(0, weight=1)
-    selectCaract.grid_rowconfigure(1, weight=1)
-    selectCaract.grid_rowconfigure(2, weight=1)
-    selectCaract.grid_rowconfigure(3, weight=1)
-    selectCaract.grid_rowconfigure(4, weight=1)
-    selectCaract.grid_columnconfigure(0, weight=1)
-    selectCaract.grid_columnconfigure(1, weight=1)
+selectCaract = Tk()
+selectCaract.title('Accueil')
+selectCaract.geometry('1300x800')
 
-    #Style Combobox
-    style= ttk.Style()
-    style.theme_use('clam')
-    style.configure("TCombobox", fieldbackground= "#edf4ed", background= "#689d71")
+#grid
+selectCaract.grid_rowconfigure(0, weight=1)
+selectCaract.grid_rowconfigure(1, weight=1)
+selectCaract.grid_rowconfigure(2, weight=1)
+selectCaract.grid_rowconfigure(3, weight=1)
+selectCaract.grid_rowconfigure(4, weight=1)
+selectCaract.grid_columnconfigure(0, weight=1)
+selectCaract.grid_columnconfigure(1, weight=1)
 
-    #Header
-    dessin=Frame(selectCaract, bg="#689d71")
-    dessin.grid(row=0,column=0,columnspan= 2,sticky="nswe")
-    #Placement des boutons
+#Style Combobox
+style= ttk.Style()
+style.theme_use('clam')
+style.configure("TCombobox", fieldbackground= "#edf4ed", background= "#689d71")
 
-    listSexe=["Homme", "Femme"]
-    listComboSexe = ttk.Combobox(selectCaract, values=listSexe)
-    listComboSexe.set("Sexe")
-    listComboSexe.grid(column=0, row=1)
+#Header
+dessin=Frame(selectCaract, bg="#689d71")
+dessin.grid(row=0,column=0,columnspan= 2,sticky="nswe")
+#Placement des boutons
 
-    listCoulCheveux=["Brun", "Gris", "Blond", "Noir", "Autre"]
-    listComboCoulCheveux = ttk.Combobox(selectCaract, values=listCoulCheveux)
-    listComboCoulCheveux.set("Couleur des cheveux")
-    listComboCoulCheveux.grid(column=1, row=1)
+listSexe=["Homme", "Femme"]
+listComboSexe = ttk.Combobox(selectCaract, values=listSexe)
+listComboSexe.set("Sexe")
+listComboSexe.grid(column=0, row=1)
 
-    listBarbe=["Imberbe", "Bouc", "Barbe de trois jours", "Moustache","Favoris", "Autre"]
-    listComboBarbe = ttk.Combobox(selectCaract, values=listBarbe)
-    listComboBarbe.set("Barbe")
-    listComboBarbe.grid(column=0, row=2)
+listCoulCheveux=["Brun", "Gris", "Blond", "Noir", "Autre"]
+listComboCoulCheveux = ttk.Combobox(selectCaract, values=listCoulCheveux)
+listComboCoulCheveux.set("Couleur des cheveux")
+listComboCoulCheveux.grid(column=1, row=1)
 
-    listCheveux=["Chauve","Lisse", "Ondulés","Calvitie", "Autre"]
-    listComboCheveux = ttk.Combobox(selectCaract, values=listCheveux)
-    listComboCheveux.set("Coupe de cheveux")
-    listComboCheveux.grid(column=1, row=2)
+listBarbe=["Imberbe", "Bouc", "Barbe de trois jours", "Moustache","Favoris", "Autre"]
+listComboBarbe = ttk.Combobox(selectCaract, values=listBarbe)
+listComboBarbe.set("Barbe")
+listComboBarbe.grid(column=0, row=2)
+
+listCheveux=["Chauve","Lisse", "Ondulés","Calvitie", "Autre"]
+listComboCheveux = ttk.Combobox(selectCaract, values=listCheveux)
+listComboCheveux.set("Coupe de cheveux")
+listComboCheveux.grid(column=1, row=2)
 
 
-    listNez=["Pointu", "Gros", "Autre"]
-    listComboNez = ttk.Combobox(selectCaract, values=listNez)
-    listComboNez.set("Forme du nez")
-    listComboNez.grid(column=0, row=3)
+listNez=["Pointu", "Gros", "Autre"]
+listComboNez = ttk.Combobox(selectCaract, values=listNez)
+listComboNez.set("Forme du nez")
+listComboNez.grid(column=0, row=3)
 
-    listSourcils=["Arqués", "Fournis"]
-    listComboSourcils = ttk.Combobox(selectCaract, values=listSourcils)
-    listComboSourcils.set("Forme des sourcils")
-    listComboSourcils.grid(column=1, row=3)
+listSourcils=["Arqués", "Fournis"]
+listComboSourcils = ttk.Combobox(selectCaract, values=listSourcils)
+listComboSourcils.set("Forme des sourcils")
+listComboSourcils.grid(column=1, row=3)
 
-    listLunettes=["Lunettes", "Pas de lunettes"]
-    listComboLunettes = ttk.Combobox(selectCaract, values=listLunettes)
-    listComboLunettes.set("Lunettes")
-    listComboLunettes.grid(column=0, row=4)
+listLunettes=["Lunettes", "Pas de lunettes"]
+listComboLunettes = ttk.Combobox(selectCaract, values=listLunettes)
+listComboLunettes.set("Lunettes")
+listComboLunettes.grid(column=0, row=4)
 
-    # bouton de sortie
+# bouton de sortie
 
-    valider=Button(selectCaract, text="Valider", command=getAll)
-    valider.grid(column=1,row=4)
+valider=Button(selectCaract, text="Valider", command=getAll)
+valider.grid(column=1,row=4)
 
-    selectCaract.configure(bg='#d0d9c8')
-    selectCaract.mainloop()
-
-SelectCaractere()
+selectCaract.configure(bg='#d0d9c8')
+selectCaract.mainloop()
