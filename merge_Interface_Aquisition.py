2ea97d4d883a7e918c0a5959e494738e76795ba4
Arnaud <arnaud.mengelle@orange.fr>
2023-03-17 13:13:36 +0100

    Interface.py fonctionne en fonctions, merge de interface et aquistion

--- merge_Interface_Aquisition.py
+++ merge_Interface_Aquisition.py
@@ -1,6 +1,25 @@
+#Importation des librairies
+import pandas as pds
+from random import randint
 from tkinter import *
 from tkinter import ttk
-from tkinter import messagebox
+
+#Importation de la table
+data = pds.read_csv('list_attr_celeba.txt', delim_whitespace=True, index_col = 0) #là j'utilise les deux listes (celle raccourcie et celle entière psk y a pas le header dans la liste raccourcie)
+#print(data)
+
+#Tri des colonnes (en ne gardant que celles que l'ont va utiliser)
+nonutilisees = ['Double_Chin', 'Bags_Under_Eyes', 'Bangs', 'Big_Lips', 'Chubby', 'High_Cheekbones', 'Narrow_Eyes', 'Attractive', 'Oval_Face', 'Pale_Skin', 'Rosy_Cheeks', 'Smiling', 'Wearing_Earrings', 'Wearing_Hat', 'Wearing_Lipstick', 'Wearing_Necklace', 'Wearing_Necktie', 'Heavy_Makeup', 'Mouth_Slightly_Open', 'Blurry']
+for i in nonutilisees:
+    data = data.drop(i, axis=1)
+
+#Voila ce qu'on obtient
+#print(data)
+
+#Pour la colonne Blurry on peut aussi decider de supprimer les lignes des images floues
+#Ca c'est une question a demander a Juliette
+
+################################# PARTIE ARNAUD #########################
 #689d71
 #edf4ed
 #a8bba0
@@ -118,6 +137,46 @@ def CaractInit():
     selectCaract.mainloop()
 
     return listAttrInit
+
+
+################################# PARTIE IMPORTATION######################
+
 dict=CaractInit()
+columns=[]
+datas=[]
 for key in dict:
-    print (dict[key])
+    column.append(key)
+    datas.append(dict[key])
+
+choix = pds.DataFrame(data=datas, columns = column, index=None)
+
+def selectionlignes(choix):
+    """
+    Entrée :
+        choix = tableau généré par l'utilisateur apres avoir choisi ses attributs
+    Sortie :
+        newdata = tableau filtré avec uniquement les lignes correspondantes à ce que l'utilisateur recherche
+    """
+    caractere = list(choix.columns.values)
+    c = '=='.join((str(caractere[0]),str(int(choix[caractere[0]]))))
+    for i in range(len(caractere)-1):
+        a = c
+        b = '=='.join((str(caractere[i+1]),str(int(choix[caractere[i+1]]))))
+        c = ' and '.join((a, b))
+    newdata = data.query(str(c))
+    return newdata
+#la je suis bloquee parce qu'il y a une erreur que je comprends pas
+
+#test
+selectionimages = selectionlignes(choix)
+
+#selection des 10 images aleatoires
+def diximages(selectionimages):
+    listeimages = list(selectionimages.index)
+    imagesdepart = []
+    for i in range(10):
+        imagesdepart.append(listeimages[randint(0, len(listeimages))])
+    return imagesdepart
+
+#test
+listeimagesdepart = diximages(selectionimages)
